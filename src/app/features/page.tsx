3058400076fdb1a54c84f1732ed44a97d5ef2942
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-11 16:33:02 +0600

    Add HoodSection and WhatsAppSection components to FeaturesPage

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,13 +1,17 @@
 /** @format */
 
 import CoreFeaturesSection from "@/components/FeaturesComponents/CoreFeaturesSection";
+import HoodSection from "@/components/FeaturesComponents/HoodSection";
 import StatsSection from "@/components/FeaturesComponents/StatsSection";
+import WhatsAppSection from "@/components/FeaturesComponents/WhatsAppSection";
 
 const FeaturesPage = () => {
   return (
     <main className="w-full bg-transparent">
       <StatsSection />
       <CoreFeaturesSection />
+      <HoodSection />
+      <WhatsAppSection />
     </main>
   );
 };
