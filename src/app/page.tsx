842080460abbec4e07fc15b0cf136e14694747f6
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-11 14:49:18 +0600

    Added PricingSection component and integrate it into Home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import FeaturesSection from "@/components/HomeComponents/FeaturesSection";
 import HeroSection from "@/components/HomeComponents/HeroSection";
+import PricingSection from "@/components/HomeComponents/PricingSection";
 import PrivacyAndControllSection from "@/components/HomeComponents/PrivacyAndControllSection";
 import SetupGuidelineSection from "@/components/HomeComponents/SetupGuidelineSection";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <SetupGuidelineSection />
       <FeaturesSection />
       <PrivacyAndControllSection />
+      <PricingSection />
     </div>
   );
 }
