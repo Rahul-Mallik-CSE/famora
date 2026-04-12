c14f2b949b7e16932ae03493a332436a985ecb8c
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-12 14:31:44 +0600

    Add SimplePricingSection component to PricingPage

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+import SimplePricingSection from "@/components/PricingComponents/SimplePricingSection";
+const PricingPage = () => {
+  return (
+    <main className="w-full bg-transparent">
+      <SimplePricingSection />
+    </main>
+  );
+};
+
+export default PricingPage;
